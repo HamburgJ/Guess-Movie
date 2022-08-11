9c21d56ac973b8ba7c34cc10d0da7fe8c8c4f8b9
Matthew Charters <matthew@charitycan.ca>
2022-08-11 11:36:31 -0400

    Add prettier and an actor tile

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,7 +1,22 @@
-import {api_key} from './Constants';
+import { api_key } from './Constants';
+import React, { useState, useEffect } from 'react';
 const base_url = 'https://api.themoviedb.org/3';
 
-export const fetchData = (route, params={}) => {
-    return fetch(`${base_url}${route}?api_key=${api_key}${Object.entries(params).map(([k, v]) => `&${k}=${v}`).join('')}`)
-        .then(response => response.json());
+export const fetchData = (route, params = {}) => {
+  return fetch(
+    `${base_url}${route}?api_key=${api_key}${Object.entries(params)
+      .map(([k, v]) => `&${k}=${v}`)
+      .join('')}`
+  ).then((response) => response.json());
+};
+
+export const useImageData = () => {
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    fetchData('/configuration').then((data) => {
+      setImages(data.images);
+    });
+  }, []);
+
+  return images;
 };
